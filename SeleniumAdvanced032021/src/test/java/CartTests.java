2462982f94ef7aacac42da6516634806eafda7ef
Mateusz <mtadla@sii.pl>
2021-03-07 15:32:52 +0100

    update

--- SeleniumAdvanced032021/src/test/java/CartTests.java
+++ SeleniumAdvanced032021/src/test/java/CartTests.java
@@ -1,5 +1,8 @@
 import model.Order;
+import org.assertj.core.api.Assertions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.cart.CartPage;
 import pages.product.ProductDetailsPage;
 import pages.product.ProductsGridComponent;
 
@@ -11,16 +14,24 @@ public class CartTests extends TestBase {
     public void shouldDisplayCorrectProductsInCart() {
         Order expectedOrder = new Order();
 
-        getDriver().get("http://5.196.7.235/");
+        for (int i = 0; i < 10; i++) {
+            getDriver().get("http://5.196.7.235/");
 
-        new ProductsGridComponent(getDriver())
-                .getRandomItem()
-                .open();
+            new ProductsGridComponent(getDriver())
+                    .getRandomItem()
+                    .open();
 
-        ProductDetailsPage prodPage = new ProductDetailsPage(getDriver());
-        prodPage.setQuantity(new Random().nextInt(5) + 1); // 1-6
-        prodPage.addToBasket(expectedOrder);
+            ProductDetailsPage prodPage = new ProductDetailsPage(getDriver());
+            prodPage.setQuantity(new Random().nextInt(5) + 1); // 1-6
+            prodPage.addToBasket(expectedOrder);
+        }
 
+        getDriver().get("http://5.196.7.235/cart");
+        CartPage cartPage = new CartPage(getDriver());
+        Order orderDisplayedInCart = cartPage.toOrder();
+
+        Assertions.assertThat(expectedOrder)
+                .isEqualToComparingFieldByFieldRecursively(orderDisplayedInCart);
     }
 
     @Test
